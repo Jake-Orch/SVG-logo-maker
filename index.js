3fd4ce3412f457bb7dc41d99f39e73dd7e5feb55
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-05 22:47:18 +0100

    branch version bug fixed for prompt

--- index.js
+++ index.js
@@ -4,8 +4,13 @@
 // then create the SVG file
 const inquirer = require('inquirer');
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+const isColor = require('is-color');
 
 inquirer.registerPrompt('max-length', MaxLengthInputPrompt);
+
+function getFontColor(data) {
+    // Get font color using data.text-color
+};
 
 function init () {
     inquirer.prompt([
@@ -18,20 +23,40 @@ function init () {
         {
             type:'input',
             message:'What font color would you like to use?',
-            name:'text-color'
+            name:'textColor',
+            validate: textColor => {
+                // isColor(textColor) ? console.log('Good choice!') : console.log('Good choice!')
+                // return
+                if (isColor(textColor)) {
+                    return true
+                } else {
+                    return 'invalid'
+                }
+            }
         },
         {
             type:'list',
             message:'Pick which shape you would like your logo to take',
             name:'shape',
-            choices:['Circle', 'Square', 'Triangle']
+            choices: ['Square', 'Circle', 'Triangle']
         },
         {
             type:'input',
             message:'What color would you like your shape to be?',
-            name:'shape-color'
+            name:'shapeColor',
+            validate: textColor => {
+                if (isColor(textColor)) {
+                    return true
+                } else {
+                    return 'invalid'
+                }
+            }
         }
     ])
+    .then((data) => {
+        //What to do with data
+        getFontColor(data);
+    })
 };
 
-init();
+init ()
