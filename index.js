1e5deb7dc899247f2cb24a0ddffdcb9067ec0f3c
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-05 21:14:57 +0100

    .gitignore fixed

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 // Create my color functions for both shape color and text color
 // Apply my colors, to the text and shape selected by the user and add them together here
 // then create the SVG file
+const inquirer = require('inquirer');
+
+function init () {
+    inquirer.prompt([
+        {
+            type:'input',
+            message:'Type 3 characters to include in your logo',
+            name:'text'
+        },
+        {
+            type:'input',
+            message:'What font color would you like to use?',
+            name:'text-color'
+        },
+        {
+            type:'choice',
+            message:'Pick which shape you would like your logo to take',
+            name:'shape'
+        },
+        {
+            type:'input',
+            message:'What color would you like your shape to be?',
+            name:'shape-color'
+        }
+    ])
+}
