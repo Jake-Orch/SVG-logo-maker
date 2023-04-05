0a9c46b30e91b34307573ea6f9e751125725196e
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-05 21:05:09 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// I am to create my inquirer input functions
+// Create my color functions for both shape color and text color
+// Apply my colors, to the text and shape selected by the user and add them together here
+// then create the SVG file
