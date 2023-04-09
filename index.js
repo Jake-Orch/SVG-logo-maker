009892f4e3cd02f676be2ae9f55abf2bdd2d7bfb
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-04-09 22:30:17 +0100

    notes added to shapes.js and index.js, also temp logo deleted

--- index.js
+++ index.js
@@ -10,21 +10,22 @@ const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 
 inquirer.registerPrompt('max-length', MaxLengthInputPrompt);
 
+//This function prompts the user with a set of 4 questions, to aquire all the necessary infotmation to create a logo how they would like
 function init() {
     inquirer.prompt([
         {
             type: 'max-length',
             message: 'Type 3 characters to include in your logo',
             name: 'text',
+            // This maxLength section, aquired by "inquirer-maxlength-input-prompt" is a good way of ensuring that the user doesnt input any more than 3 characters when making their logo
             maxLength: 3
         },
         {
             type: 'input',
             message: 'What font color would you like to use?',
             name: 'textColor',
+            // This if statement is to make sure that the color the user types in is infact a color, so that the file can be created properly
             validate: textColor => {
-                // isColor(textColor) ? console.log('Good choice!') : console.log('Good choice!')
-                // return
                 if (isColor(textColor)) {
                     return true
                 } else {
@@ -42,6 +43,7 @@ function init() {
             type: 'input',
             message: 'What color would you like your shape to be?',
             name: 'shapeColor',
+            // This if statement is to make sure that the color the user types in is infact a color, so that the file can be created properly
             validate: textColor => {
                 if (isColor(textColor)) {
                     return true
@@ -54,17 +56,19 @@ function init() {
         .then((data) => {
             //What to do with data
             console.log(data)
+            // Here i am deconstructing the data, into 4 variables to be used to create the logo
             const {shape, shapeColor, text, textColor} = data;
+            //  newLogo is a variable version of shapeChecker(shape, shapeColor, text, textColor).makeLogo(), to be used when writing the .svg file
             const newLogo = shapeChecker(shape, shapeColor, text, textColor).makeLogo()
-//            shapeAssembler(shape, newLogo)
             console.log(newLogo)
+            // This function creates the "logo.svg" file with the specific requirements stated by the user
             fs.writeFile('logo.svg', newLogo, (e) => {
                 e ? console.log(e) : console.log('Logo saved as "logo.svg"')
             });
         })
 };
 
-
+// This function filters through the possible shapes selected by the user, then uses the classes from ./lib/shapes.js to create their desired logo, using the rest of the data the user inputs
 function shapeChecker (shape, shapeColor, text, textColor) {
         if (shape == 'Square') {
        return new Square(shapeColor, text, textColor)
@@ -75,16 +79,5 @@ function shapeChecker (shape, shapeColor, text, textColor) {
     }
 }
 
-// function shapeAssembler (shape, newLogo) {
-//     if (shape == 'Square') {
-//    return `<svg version="1.1"
-//    width="300" height="200"
-//    xmlns="http://www.w3.org/2000/svg">` + newLogo + 
-// } else if (shape == 'Circle') {
-//    return 
-// } else {
-//    return 
-// }
-// }
-
+// This calles the init function, launching the prompts in the terminal
 init()
